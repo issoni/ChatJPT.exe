33a276b71b241494bca320baaf111fb0c02acc5a
Ishika Soni <ishikasoni50@gmail.com>
2024-07-24 19:59:31 -0400

    efficient way of showing text

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -9,14 +9,12 @@ public class MainMenuManager : MonoBehaviour
     public Text bootText1;
     public Text bootText2;
     public Text bootText3;
+    public Text bootText4; 
     public GameObject menuPanel;
 
     // Start is called before the first frame update
     private void Start()
     {
-        bootText1.gameObject.SetActive(false);
-        bootText2.gameObject.SetActive(false);
-        bootText3.gameObject.SetActive(false);
         menuPanel.SetActive(false); //menu is hidden at the start
         StartCoroutine(BootSequence()); 
     }
@@ -24,19 +22,24 @@ public class MainMenuManager : MonoBehaviour
     private IEnumerator BootSequence()
     {
         //shows first line of text
-        bootText1.gameObject.SetActive(true);
+        bootText1.text = "Initializing...";
         yield return new WaitForSeconds(2f); //wait for 2 secs
 
         //shows second line of text
-        bootText2.gameObject.SetActive(true);
+        bootText2.text = "Loading system files...";
         yield return new WaitForSeconds(2f); //wait for 2 secs
 
         //shows third line of text
-        bootText3.gameObject.SetActive(true);
+        bootText3.text = "Press DEL to enter setup"; 
         yield return new WaitForSeconds(2f); //wait for 2 secs
 
         //cursor blinking effect
         StartCoroutine(BlinkCursor());
+
+        yield return new WaitUntil(() => Input.anyKeyDown);
+
+        //stop cursor blinking
+        StopCoroutine(BlinkCursor()); 
 
         //hide the boot texts and show menu
         bootText1.gameObject.SetActive(false);
@@ -47,12 +50,12 @@ public class MainMenuManager : MonoBehaviour
 
     private IEnumerator BlinkCursor()
     {
-        Text cursor = bootText3;
+        Text cursor = bootText4;
         while (true)
         {
             cursor.text = "_";
             yield return new WaitForSeconds(0.5f); //blinking duration
-            cursor.text = ".";
+            cursor.text = "";
             yield return new WaitForSeconds(0.5f); 
         }
 
