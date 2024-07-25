af6be4e6290c3d617bc2ddf573baf73a9377809a
Ishika Soni <ishikasoni50@gmail.com>
2024-07-24 20:15:41 -0400

    boot sequence working with a cursor blinking and an integer increasing

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -9,14 +9,16 @@ public class MainMenuManager : MonoBehaviour
     public Text bootText1;
     public Text bootText2;
     public Text bootText3;
-    public Text bootText4; 
+    public Text bootText4;
+    public Text bootText5; 
     public GameObject menuPanel;
 
     // Start is called before the first frame update
     private void Start()
     {
         menuPanel.SetActive(false); //menu is hidden at the start
-        StartCoroutine(BootSequence()); 
+        StartCoroutine(BootSequence());
+        bootText5.gameObject.SetActive(false);
     }
 
     private IEnumerator BootSequence()
@@ -33,18 +35,25 @@ public class MainMenuManager : MonoBehaviour
         bootText3.text = "Press DEL to enter setup"; 
         yield return new WaitForSeconds(2f); //wait for 2 secs
 
+        //start integer increment effect
+        bootText5.gameObject.SetActive(true);
+        StartCoroutine(IncrementInteger());
+
         //cursor blinking effect
         StartCoroutine(BlinkCursor());
 
         yield return new WaitUntil(() => Input.anyKeyDown);
 
         //stop cursor blinking
-        StopCoroutine(BlinkCursor()); 
+        StopCoroutine(BlinkCursor());
+        StopCoroutine(IncrementInteger()); 
 
         //hide the boot texts and show menu
         bootText1.gameObject.SetActive(false);
         bootText2.gameObject.SetActive(false);
         bootText3.gameObject.SetActive(false);
+        bootText4.gameObject.SetActive(false);
+        bootText5.gameObject.SetActive(false);
         menuPanel.SetActive(true); 
     }
 
@@ -59,6 +68,17 @@ public class MainMenuManager : MonoBehaviour
             yield return new WaitForSeconds(0.5f); 
         }
 
+    }
+
+    private IEnumerator IncrementInteger()
+    {
+        int counter = 23048;
+        while (true)
+        {
+            bootText5.text = counter.ToString();
+            counter += Random.Range(200, 800); //increases by a random value
+            yield return new WaitForSeconds(0.1f); //updates every 0.1 secs 
+        }
     }
     
 }
