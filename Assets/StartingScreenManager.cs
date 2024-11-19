b583e42954c8d7c1365bfaecb32b67e45f1e6581
Ishika Soni <ishikasoni50@gmail.com>
2024-11-19 17:06:47 -0500

    save

--- Assets/StartingScreenManager.cs
+++ Assets/StartingScreenManager.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class StartingScreenManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
-        
+       
+        Debug.Log("Playing animation: COS-cherries-2");
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
