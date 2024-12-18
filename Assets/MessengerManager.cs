bab65e725d7807364a387e7ae908d4b4fc6874aa
Ishika Soni <ishikasoni50@gmail.com>
2024-12-18 14:13:51 -0500

    messenger screen working -cleaning it up

--- Assets/MessengerManager.cs
+++ Assets/MessengerManager.cs
@@ -1,18 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; 
 
 public class MessengerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_InputField inputField;
+    public GameObject messagesContainer;
+    public GameObject playerMessagePrefab;
+    public GameObject npcMessagePrefab;
+    public List<string> playerPrewrittenTexts;
+    public List<string> npcPrewrittenTexts;
+
+    private int playerTextIndex = 0;
+    private int npcTextIndex = 0;
+    private bool isPlayerTurn = true;
+    private bool isTypingComplete = false; 
+
+
     void Start()
     {
-        
+        inputField.text = "";
+        inputField.readOnly = true; 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isPlayerTurn && playerTextIndex < playerPrewrittenTexts.Count)
+        {
+            if (Input.anyKeyDown && !IsMouseInput() && inputField.text.Length < playerPrewrittenTexts[playerTextIndex].Length)
+            {
+                string currentText = playerPrewrittenTexts[playerTextIndex].Substring(0, inputField.text.Length + 1);
+                inputField.text = currentText;
+                inputField.caretPosition = inputField.text.Length;
+
+                if (inputField.text == playerPrewrittenTexts[playerTextIndex])
+                {
+                    isTypingComplete = true;
+                }
+            }
+
+
+            if (isTypingComplete && Input.GetKeyDown(KeyCode.Return))
+            {
+                SendMessage(true, playerPrewrittenTexts[playerTextIndex]);
+                playerTextIndex++;
+                StartCoroutine(ScheduleNPCResponse());
+                ResetPlayerInput(); 
+            }
+        }
+    }
+
+    void SendMessage(bool isPlayer, string messageText)
+    {
+        GameObject messagePrefab = isPlayer ? playerMessagePrefab : npcMessagePrefab;
+
+        GameObject newMessage = Instantiate(messagePrefab, messagesContainer.transform);
+        TMP_Text messageContent = newMessage.GetComponent<TMP_Text>();
+        messageContent.text = messageText;
+
+        Canvas.ForceUpdateCanvases();
+        messagesContainer.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0); 
+    }
+
+    IEnumerator ScheduleNPCResponse()
+    {
+        isPlayerTurn = false;
+        yield return new WaitForSeconds(2f);
+
+        if (npcTextIndex < npcPrewrittenTexts.Count)
+        {
+            SendMessage(false, npcPrewrittenTexts[npcTextIndex]);
+            npcTextIndex++; 
+        }
+
+        yield return new WaitForSeconds(1f);
+        isPlayerTurn = true; 
+    }
+
+    void ResetPlayerInput()
+    {
+        inputField.text = "";
+        inputField.caretPosition = 0;
+        isTypingComplete = false; 
+    }
+
+    private bool IsMouseInput()
+    {
+        return Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2);
     }
 }
